d6bb1d9a469c02671268a0f9e0ec6f1f86d4c02c
BravoChos <bravochos@gmail.com>
2024-12-28 16:57:31 +0900

    add basic header on top

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1 className="text-3xl font-bold underline bg-red-100">Hello world!</h1>
       <div className="bg-blue-500 text-white font-bold p-4 text-center">Tailwind CSS가 정상적으로 동작합니다!</div>
       <div className="bg-blue-500 text-white p-5">Tailwind 작동 확인</div>
